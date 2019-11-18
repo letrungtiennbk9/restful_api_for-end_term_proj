7cbfd0947e8b975b85c585eb419248ccf53f9bcd
letrungtiennbk9 <letrungtiennbk9@gmail.com>
2019-11-18 16:46:04 +0700

    a

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log("a");
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
