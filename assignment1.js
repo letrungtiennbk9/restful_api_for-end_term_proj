8cb1e8e683cad1dd7d421389d561218476163f1e
letrungtiennbk9 <47937082+letrungtiennbk9@users.noreply.github.com>
2019-11-18 15:20:57 +0700

    Update assignment1.js
    change port

--- assignment1.js
+++ assignment1.js
@@ -59,4 +59,4 @@ MongoClient.connect(uri, {autoIndex: false} ,(err, result) => {
   }
 });
 
-app.listen("https://restful-api-for-end-term-proj.herokuapp.com/");
+app.listen("https://myrestaurantand.herokuapp.com/index.html");
